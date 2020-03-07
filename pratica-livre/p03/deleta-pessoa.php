d4c609283c430404fa755e098ef46d0160414a75
gabrielssa <gabriel.schubert.araujo@ccc.ufcg.edu.br>
2020-03-06 23:47:13 -0300

    crud em php, testando

---
+++ pratica-livre/p03/deleta-pessoa.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "127.0.0.1";
+$username = "pmauser";
+$password = "ax1v09p2";
+$dbname = "test";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// sql to delete a record
+$id = $_POST['id'];
+
+$sql = "DELETE FROM tb_pessoa WHERE id='$id'";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Record deleted successfully";
+} else {
+    echo "Error deleting record: " . $conn->error;
+}
+echo '<a href="index.html">Voltar</a><br>';
+$conn->close();
+?> 
--- pratica-livre/p03/lista-pessoas.php
+++ pratica-livre/p03/lista-pessoas.php
@@ -30,6 +30,8 @@ if ($result->num_rows > 0) {
 } else {
     echo "0 results";
 }
+
+echo "<a href=\"index.html\">Voltar</a>";
 $conn->close();
 ?> 
 </body>
